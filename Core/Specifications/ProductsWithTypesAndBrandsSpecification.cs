826b2edb3e969c041fea24f210afc2751da8766e
MatthewHufft <matthufft777@gmail.com>
2022-06-13 13:20:24 -0600

    sort products by price asc and desc

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,18 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
 using Core.Entities;
 
 namespace Core.Specifications
 {
 	public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
 	{
-		public ProductsWithTypesAndBrandsSpecification()
+		public ProductsWithTypesAndBrandsSpecification(string sort)
 		{
 			AddInclude(x => x.ProductType);
 			AddInclude(x => x.ProductBrand);
+			AddOrderBy(x => x.Name);
+
+			if (!string.IsNullOrEmpty(sort))
+			{
+				switch (sort)
+				{
+					case "priceAsc":
+						AddOrderBy(p => p.Price);
+						break;
+					case "priceDesc":
+						AddOrderByDescending(p => p.Price);
+						break;
+					default:
+						AddOrderBy(n => n.Name);
+						break;
+				}
+			}
 		}
 
 		public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
